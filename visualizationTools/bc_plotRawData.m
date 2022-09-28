c04d756293484a5bb8168361adf7322e04dd3145
Julie-Fabre <julie.m.fabre@gmail.com>
2022-09-28 10:36:58 +0100

    explore raw data

--- visualizationTools/bc_plotRawData.m
+++ visualizationTools/bc_plotRawData.m
@@ -1,6 +1,6 @@
 %% info
 % data
-ephysPath = '/home/netshare/zaru/JF082/2022-07-25/ephys/kilosort2/site1';%pathToFolderYourEphysDataIsIn; % eg /home/netshare/zinu/JF067/2022-02-17/ephys/kilosort2/site1, whre this path contains 
+ephysPath = '/media/julie/Elements/dataset/kilosort2/';%pathToFolderYourEphysDataIsIn; % eg /home/netshare/zinu/JF067/2022-02-17/ephys/kilosort2/site1, whre this path contains 
 
 % channels to plot
 channelsToPlot = 1:20;
@@ -16,12 +16,11 @@ ephysData.spike_templates = spikeTemplates;
 ephysData.templates = templateWaveforms;
 ephysData.template_amplitudes = templateAmplitudes;
 ephysData.channel_positions = channelPositions;
-
 ephysData.waveform_t = 1e3*((0:size(templateWaveforms, 2) - 1) / 30000);
 
 
 %% get memmap
-param.tmpFolder = ephysPath;
+param.tmpFolder = strrep(ephysPath, 'kilosort2/', '');
 param.nChannels = 385;
 bc_getRawMemMap;
 
