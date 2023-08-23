f72580c62e0c09978cfbd5f61f93b747e2491d9c
Julie-Fabre <julie.m.fabre@gmail.com>
2023-08-23 16:24:55 +0100

    prettify plots: homogenize x and y limits, row / col -wise

--- prettify_plot.m
+++ prettify_plot.m
@@ -1,16 +1,29 @@
-function prettify_plot(sameX, sameY, inputColor, titleFontSize, labelFontSize, generalFontSize, pointSize, lineThickness, fontColor)
-    
-    
+function prettify_plot(sameXLimits, sameYLimits, figureColor, titleFontSize, labelFontSize, generalFontSize, pointSize, lineThickness, textColor)
+% make current figure pretty
+% ------
+% Inputs
+% ------
+% - sameXLimits
+% - sameYLimits
+% - figureColor
+% - titleFontSize
+% - labelFontSize
+% - generalFontSize
+% - pointSize
+% - lineThickness
+% - textColor
+% ------
+% Julie M. J. Fabre
 
     % Set default parameter values
-    if nargin < 1 || isempty(sameX)
-        sameX = 'all';
+    if nargin < 1 || isempty(sameXLimits)
+        sameXLimits = 'all';
     end
-    if nargin < 2 || isempty(sameY)
-        sameY = 'all';
+    if nargin < 2 || isempty(sameYLimits)
+        sameYLimits = 'all';
     end
-    if nargin < 3 || isempty(inputColor)
-        inputColor = 'w';
+    if nargin < 3 || isempty(figureColor)
+        figureColor = 'w';
     end
     if nargin < 4 || isempty(titleFontSize)
         titleFontSize = 18;
@@ -27,15 +40,15 @@ function prettify_plot(sameX, sameY, inputColor, titleFontSize, labelFontSize, g
     if nargin < 8 || isempty(lineThickness)
         lineThickness = 2;
     end
-    if nargin < 9 || isempty(fontColor)
+    if nargin < 9 || isempty(textColor)
         % Set default font color based on the input color
-        switch inputColor
+        switch figureColor
             case 'k'
-                fontColor = 'w';
+                textColor = 'w';
             case 'none'
-                fontColor = [0.7, 0.7, 0.7]; % Gray
+                textColor = [0.7, 0.7, 0.7]; % Gray
             otherwise
-                fontColor = 'k';
+                textColor = 'k';
         end
     end
     
@@ -44,46 +57,84 @@ function prettify_plot(sameX, sameY, inputColor, titleFontSize, labelFontSize, g
     
     
     % Set color properties for figure and axis
-    set(currFig, 'color', inputColor);
+    set(currFig, 'color', figureColor);
     
-    
-    for iAx = 1:size(currFig.Children,1)
-        currAx = currFig.Children(iAx);
-        set(currAx, 'color', inputColor);
-        
-        % Set font properties for the axis
-        set(currAx.XLabel, 'FontSize', labelFontSize, 'Color', fontColor);
-        set(currAx.YLabel, 'FontSize', labelFontSize, 'Color', fontColor);
-        set(currAx.Title, 'FontSize', titleFontSize, 'Color', fontColor);
-        set(currAx, 'FontSize', generalFontSize, 'GridColor', fontColor, ...
-                    'YColor', fontColor, 'XColor', fontColor, ...
-                    'MinorGridColor', fontColor);
-        
-        % Adjust properties of line children within the plot
-        childLines = findall(currAx, 'Type', 'line');
-        for thisLine = childLines'
-            if strcmp('.', get(thisLine, 'Marker'))
-                set(thisLine, 'MarkerSize', pointSize);
+    % get axes children 
+    all_axes = find(arrayfun(@(x) contains(currFig.Children(x).Type, 'axes'), 1:size(currFig.Children,1)));
+
+    for iAx = 1:size(all_axes,2)
+        thisAx = all_axes(iAx);
+        currAx = currFig.Children(thisAx);
+        set(currAx, 'color', figureColor);
+        if ~isempty(currAx)
+            % Set font properties for the axis
+            set(currAx.XLabel, 'FontSize', labelFontSize, 'Color', textColor);
+            set(currAx.YLabel, 'FontSize', labelFontSize, 'Color', textColor);
+            set(currAx.Title, 'FontSize', titleFontSize, 'Color', textColor);
+            set(currAx, 'FontSize', generalFontSize, 'GridColor', textColor, ...
+                        'YColor', textColor, 'XColor', textColor, ...
+                        'MinorGridColor', textColor);
+            
+            % Adjust properties of line children within the plot
+            childLines = findall(currAx, 'Type', 'line');
+            for thisLine = childLines'
+                if strcmp('.', get(thisLine, 'Marker'))
+                    set(thisLine, 'MarkerSize', pointSize);
+                end
+                if strcmp('-', get(thisLine, 'LineStyle'))
+                    set(thisLine, 'LineWidth', lineThickness);
+                end
             end
-            if strcmp('-', get(thisLine, 'LineStyle'))
-                set(thisLine, 'LineWidth', lineThickness);
-            end
+            
+            % Get x and y limits 
+            xlims_subplot(iAx,:) = get(gca, 'XLim');
+            ylims_subplot(iAx,:) = get(gca, 'YLim');
+
+            % Get plot position
+            pos_subplot(iAx,:) = currAx.Position(1:2); % [left bottom width height]
         end
-        xlims_subplot(iAx,:) = xlim;
-        ylims_subplot(iAx,:) = ylim;
     end
+
+
     % make x and y lims the same 
-    if ismember(sameX, {'all'}) || ismember(sameY, {'all'})
-        for iAx = 1:size(currFig.Children,1)
-            currAx = currFig.Children(iAx);
-            if ismember(sameX, {'all'})
-                set(currAx, 'Xlim', [ min(min(xlims_subplot)), max(max(xlims_subplot))]);
-            end
-            if ismember(sameY, {'all'})
-                set(currAx, 'Ylim', [ min(min(ylims_subplot)), max(max(ylims_subplot))]);
+    if ismember(sameXLimits, {'all', 'row', 'col'}) || ismember(sameYLimits, {'all', 'row', 'col'})
+        % get rows and cols 
+        col_subplots = unique(pos_subplot(:,1));
+        row_subplots = unique(pos_subplot(:,2));
+
+        col_xlims = arrayfun(@(x) [min(min(xlims_subplot(pos_subplot(:,1)==col_subplots(x),:))),...
+            max(max(xlims_subplot(pos_subplot(:,1)==col_subplots(x),:)))], 1:size(col_subplots,1), 'UniformOutput', false);
+        row_xlims = arrayfun(@(x) [min(min(xlims_subplot(pos_subplot(:,2)==row_subplots(x),:))),...
+            max(max(xlims_subplot(pos_subplot(:,2)==row_subplots(x),:)))], 1:size(row_subplots,1), 'UniformOutput', false);
+        col_ylims = arrayfun(@(x) [min(min(ylims_subplot(pos_subplot(:,1)==col_subplots(x),:))),...
+            max(max(ylims_subplot(pos_subplot(:,1)==col_subplots(x),:)))], 1:size(col_subplots,1), 'UniformOutput', false);
+        row_ylims = arrayfun(@(x) [min(min(ylims_subplot(pos_subplot(:,2)==row_subplots(x),:))),...
+            max(max(ylims_subplot(pos_subplot(:,2)==row_subplots(x),:)))], 1:size(row_subplots,1), 'UniformOutput', false);
+
+        for iAx = 1:size(all_axes,2)
+            thisAx = all_axes(iAx);
+            currAx = currFig.Children(thisAx);
+            if ~isempty(currAx)
+                if ismember(sameXLimits, {'all'})
+                    set(currAx, 'Xlim', [ min(min(xlims_subplot)), max(max(xlims_subplot))]);
+                end
+                if ismember(sameYLimits, {'all'})
+                    set(currAx, 'Ylim', [ min(min(ylims_subplot)), max(max(ylims_subplot))]);
+                end
+                if ismember(sameXLimits, {'row'})
+                    set(currAx, 'Xlim', row_xlims{pos_subplot(iAx,2)==row_subplots});
+                end
+                if ismember(sameYLimits, {'row'})
+                    set(currAx, 'Ylim', row_ylims{pos_subplot(iAx,2)==row_subplots});
+                end
+                if ismember(sameXLimits, {'col'})
+                    set(currAx, 'Xlim', col_xlims{pos_subplot(iAx,1)==col_subplots});
+                end
+                if ismember(sameYLimits, {'col'})
+                    set(currAx, 'Ylim', col_ylims{pos_subplot(iAx,1)==col_subplots});
+                end
             end
         end
-        
     end
 end
 
