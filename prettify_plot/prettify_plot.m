9fd5c6eb4a55b04e93fafa6a13a52e722e4bcd01
Julie-Fabre <julie.m.fabre@gmail.com>
2023-11-02 11:45:51 +0000

    fix prettifying legend bug

--- prettify_plot/prettify_plot.m
+++ prettify_plot/prettify_plot.m
@@ -4,20 +4,20 @@ function prettify_plot(sameXLimits, sameYLimits, figureColor, legendAsTxt, title
 % ------
 % Inputs
 % ------
-% - sameXLimits: string. Either: 
-%       - 'none': don't change any of the xlimits 
-%       - 'all': set all xlimits to the same values 
+% - sameXLimits: string. Either:
+%       - 'none': don't change any of the xlimits
+%       - 'all': set all xlimits to the same values
 %       - 'row': set all xlimits to the same values for each subplot row
 %       - 'col': set all xlimits to the same values for each subplot col
-% - sameYLimits: string. Either: 
-%       - 'none': don't change any of the xlimits 
-%       - 'all': set all xlimits to the same values 
+% - sameYLimits: string. Either:
+%       - 'none': don't change any of the xlimits
+%       - 'all': set all xlimits to the same values
 %       - 'row': set all xlimits to the same values for each subplot row
 %       - 'col': set all xlimits to the same values for each subplot col
 % - figureColor: string (e.g. 'w', 'k', ..) or RGB value defining the plots
-%       background color. 
+%       background color.
 % - legendAsTxt: 1 if you want the legend box to be replace by text
-%       directly plotted on the figure, next to the each subplot's line/point 
+%       directly plotted on the figure, next to the each subplot's line/point
 % - titleFontSize: double
 % - labelFontSize: double
 % - generalFontSize: double
@@ -26,84 +26,85 @@ function prettify_plot(sameXLimits, sameYLimits, figureColor, legendAsTxt, title
 % - textColor: double
 % ------
 % to do:
-% - option to adjust vertical and horiz. lines 
+% - option to adjust vertical and horiz. lines
 % - padding
 % - fit data to plot (adjust lims)
-% - font 
-% - padding / suptitles 
+% - font
+% - padding / suptitles
 % ------
 % Julie M. J. Fabre
 
-    % Set default parameter values
-    if nargin < 1 || isempty(sameXLimits)
-        sameXLimits = 'none';
+% Set default parameter values
+if nargin < 1 || isempty(sameXLimits)
+    sameXLimits = 'none';
+end
+if nargin < 2 || isempty(sameYLimits)
+    sameYLimits = 'none';
+end
+if nargin < 3 || isempty(figureColor)
+    figureColor = 'w';
+end
+if nargin < 4 || isempty(legendAsTxt)
+    legendAsTxt = 0;
+end
+if nargin < 5 || isempty(titleFontSize)
+    titleFontSize = 18;
+end
+if nargin < 6 || isempty(labelFontSize)
+    labelFontSize = 15;
+end
+if nargin < 7 || isempty(generalFontSize)
+    generalFontSize = 13;
+end
+if nargin < 8 || isempty(pointSize)
+    pointSize = 15;
+end
+if nargin < 9 || isempty(lineThickness)
+    lineThickness = 2;
+end
+if nargin < 10 || isempty(textColor)
+    % Set default font color based on the input color
+    switch figureColor
+        case 'k'
+            textColor = 'w';
+        case 'none'
+            textColor = [0.7, 0.7, 0.7]; % Gray
+        otherwise
+            textColor = 'k';
     end
-    if nargin < 2 || isempty(sameYLimits)
-        sameYLimits = 'none';
-    end
-    if nargin < 3 || isempty(figureColor)
-        figureColor = 'w';
-    end
-    if nargin < 4 || isempty(legendAsTxt)
-        legendAsTxt = 0;
-    end
-    if nargin < 5 || isempty(titleFontSize)
-        titleFontSize = 18;
-    end
-    if nargin < 6 || isempty(labelFontSize)
-        labelFontSize = 15;
-    end
-    if nargin < 7 || isempty(generalFontSize)
-        generalFontSize = 13;
-    end
-    if nargin < 8 || isempty(pointSize)
-        pointSize = 15;
-    end
-    if nargin < 9 || isempty(lineThickness)
-        lineThickness = 2;
-    end
-    if nargin < 10 || isempty(textColor)
-        % Set default font color based on the input color
-        switch figureColor
-            case 'k'
-                textColor = 'w';
-            case 'none'
-                textColor = [0.7, 0.7, 0.7]; % Gray
-            otherwise
-                textColor = 'k';
-        end
-    end
-    
-    % Get handles for current figure and axis
-    currFig = gcf;
-    
-    
-    % Set color properties for figure and axis
-    set(currFig, 'color', figureColor);
-    
-    % get axes children 
-    all_axes = find(arrayfun(@(x) contains(currFig.Children(x).Type, 'axes'), 1:size(currFig.Children,1)));
+end
 
-    for iAx = 1:size(all_axes,2)
-        thisAx = all_axes(iAx);
-        currAx = currFig.Children(thisAx);
-        set(currAx, 'color', figureColor);
-        if ~isempty(currAx)
-            % Set font properties for the axis
+% Get handles for current figure and axis
+currFig = gcf;
+
+
+% Set color properties for figure and axis
+set(currFig, 'color', figureColor);
+
+% get axes children
+all_axes = find(arrayfun(@(x) contains(currFig.Children(x).Type, 'axes'), 1:size(currFig.Children, 1)));
+
+for iAx = 1:size(all_axes, 2)
+    thisAx = all_axes(iAx);
+    currAx = currFig.Children(thisAx);
+    set(currAx, 'color', figureColor);
+    if ~isempty(currAx)
+        % Set font properties for the axis
+        try
             set(currAx.XLabel, 'FontSize', labelFontSize, 'Color', textColor);
-            if strcmp(currAx.YAxisLocation, 'left')% if there is both a left and right yaxis, keep the colors 
+            if strcmp(currAx.YAxisLocation, 'left') % if there is both a left and right yaxis, keep the colors
                 set(currAx.YLabel, 'FontSize', labelFontSize);
             else
                 set(currAx.YLabel, 'FontSize', labelFontSize, 'Color', textColor);
             end
             set(currAx.Title, 'FontSize', titleFontSize, 'Color', textColor);
             set(currAx, 'FontSize', generalFontSize, 'GridColor', textColor, ...
-                        'YColor', textColor, 'XColor', textColor, ...
-                        'MinorGridColor', textColor);
+                'YColor', textColor, 'XColor', textColor, ...
+                'MinorGridColor', textColor);
             if ~isempty(currAx.Legend)
                 set(currAx.Legend, 'Color', figureColor, 'TextColor', textColor)
             end
-            
+
             % Adjust properties of line children within the plot
             childLines = findall(currAx, 'Type', 'line');
             for thisLine = childLines'
@@ -125,70 +126,75 @@ function prettify_plot(sameXLimits, sameYLimits, figureColor, legendAsTxt, title
                     set(thisErrBar, 'LineWidth', lineThickness);
                 end
             end
-            
-            % Get x and y limits 
-            xlims_subplot(iAx,:) = currAx.XLim;
-            ylims_subplot(iAx,:) = currAx.YLim;
+
+            % Get x and y limits
+            xlims_subplot(iAx, :) = currAx.XLim;
+            ylims_subplot(iAx, :) = currAx.YLim;
 
             % Get plot position
-            pos_subplot(iAx,:) = currAx.Position(1:2); % [left bottom width height
+            pos_subplot(iAx, :) = currAx.Position(1:2); % [left bottom width height
             if ~isempty(currAx.Legend)
-                if legendAsTxt ==1 
+                if legendAsTxt == 1
                     prettify_legend(currAx)
                 else
-                    legend('Location','best')
+                    legend('Location', 'best')
                 end
             end
-        end
-    end
-
-
-    % make x and y lims the same 
-    if ismember(sameXLimits, {'all', 'row', 'col'}) || ismember(sameYLimits, {'all', 'row', 'col'})
-        % get rows and cols 
-        col_subplots = unique(pos_subplot(:,1));
-        row_subplots = unique(pos_subplot(:,2));
-
-        col_xlims = arrayfun(@(x) [min(min(xlims_subplot(pos_subplot(:,1)==col_subplots(x),:))),...
-            max(max(xlims_subplot(pos_subplot(:,1)==col_subplots(x),:)))], 1:size(col_subplots,1), 'UniformOutput', false);
-        row_xlims = arrayfun(@(x) [min(min(xlims_subplot(pos_subplot(:,2)==row_subplots(x),:))),...
-            max(max(xlims_subplot(pos_subplot(:,2)==row_subplots(x),:)))], 1:size(row_subplots,1), 'UniformOutput', false);
-        col_ylims = arrayfun(@(x) [min(min(ylims_subplot(pos_subplot(:,1)==col_subplots(x),:))),...
-            max(max(ylims_subplot(pos_subplot(:,1)==col_subplots(x),:)))], 1:size(col_subplots,1), 'UniformOutput', false);
-        row_ylims = arrayfun(@(x) [min(min(ylims_subplot(pos_subplot(:,2)==row_subplots(x),:))),...
-            max(max(ylims_subplot(pos_subplot(:,2)==row_subplots(x),:)))], 1:size(row_subplots,1), 'UniformOutput', false);
-
-        for iAx = 1:size(all_axes,2)
-            thisAx = all_axes(iAx);
-            currAx = currFig.Children(thisAx);
-            if ~isempty(currAx)
-                if ismember(sameXLimits, {'all'})
-                    set(currAx, 'Xlim', [ min(min(xlims_subplot)), max(max(xlims_subplot))]);
-                end
-                if ismember(sameYLimits, {'all'})
-                    set(currAx, 'Ylim', [ min(min(ylims_subplot)), max(max(ylims_subplot))]);
-                end
-                if ismember(sameXLimits, {'row'})
-                    set(currAx, 'Xlim', row_xlims{pos_subplot(iAx,2)==row_subplots});
-                end
-                if ismember(sameYLimits, {'row'})
-                    set(currAx, 'Ylim', row_ylims{pos_subplot(iAx,2)==row_subplots});
-                end
-                if ismember(sameXLimits, {'col'})
-                    set(currAx, 'Xlim', col_xlims{pos_subplot(iAx,1)==col_subplots});
-                end
-                if ismember(sameYLimits, {'col'})
-                    set(currAx, 'Ylim', col_ylims{pos_subplot(iAx,1)==col_subplots});
-                end
-            end
-            % set legend position
-            if legendAsTxt ==1 
-                prettify_legend(currAx)
-            else
-                legend('Location','best')
-            end
+        catch
         end
     end
 end
 
 
+% make x and y lims the same
+if ismember(sameXLimits, {'all', 'row', 'col'}) || ismember(sameYLimits, {'all', 'row', 'col'})
+    % get rows and cols
+    col_subplots = unique(pos_subplot(:, 1));
+    row_subplots = unique(pos_subplot(:, 2));
+
+    col_xlims = arrayfun(@(x) [min(min(xlims_subplot(pos_subplot(:, 1) == col_subplots(x), :))), ...
+        max(max(xlims_subplot(pos_subplot(:, 1) == col_subplots(x), :)))], 1:size(col_subplots, 1), 'UniformOutput', false);
+    row_xlims = arrayfun(@(x) [min(min(xlims_subplot(pos_subplot(:, 2) == row_subplots(x), :))), ...
+        max(max(xlims_subplot(pos_subplot(:, 2) == row_subplots(x), :)))], 1:size(row_subplots, 1), 'UniformOutput', false);
+    col_ylims = arrayfun(@(x) [min(min(ylims_subplot(pos_subplot(:, 1) == col_subplots(x), :))), ...
+        max(max(ylims_subplot(pos_subplot(:, 1) == col_subplots(x), :)))], 1:size(col_subplots, 1), 'UniformOutput', false);
+    row_ylims = arrayfun(@(x) [min(min(ylims_subplot(pos_subplot(:, 2) == row_subplots(x), :))), ...
+        max(max(ylims_subplot(pos_subplot(:, 2) == row_subplots(x), :)))], 1:size(row_subplots, 1), 'UniformOutput', false);
+
+    for iAx = 1:size(all_axes, 2)
+        thisAx = all_axes(iAx);
+        currAx = currFig.Children(thisAx);
+        if ~isempty(currAx)
+            try
+                if ismember(sameXLimits, {'all'})
+                    set(currAx, 'Xlim', [min(min(xlims_subplot)), max(max(xlims_subplot))]);
+                end
+                if ismember(sameYLimits, {'all'})
+                    set(currAx, 'Ylim', [min(min(ylims_subplot)), max(max(ylims_subplot))]);
+                end
+                if ismember(sameXLimits, {'row'})
+                    set(currAx, 'Xlim', row_xlims{pos_subplot(iAx, 2) == row_subplots});
+                end
+                if ismember(sameYLimits, {'row'})
+                    set(currAx, 'Ylim', row_ylims{pos_subplot(iAx, 2) == row_subplots});
+                end
+                if ismember(sameXLimits, {'col'})
+                    set(currAx, 'Xlim', col_xlims{pos_subplot(iAx, 1) == col_subplots});
+                end
+                if ismember(sameYLimits, {'col'})
+                    set(currAx, 'Ylim', col_ylims{pos_subplot(iAx, 1) == col_subplots});
+                end
+            catch
+            end
+        end
+        % set legend position
+        if ~isempty(currAx.Legend)
+            if legendAsTxt == 1
+                prettify_legend(currAx)
+            else
+                legend('Location', 'best')
+            end
+        end
+    end
+end
+end
