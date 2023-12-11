c442338c3a173e641c29f9a8d0313f291e4779e3
Julie-Fabre <julie.m.fabre@gmail.com>
2023-12-11 11:50:57 +0000

    fix loading pcfeatures

--- visualizationTools/bc_upSetPlot.m
+++ visualizationTools/bc_upSetPlot.m
@@ -1,32 +1,89 @@
-function bc_upSetPlot(qMetric, param, qMetricNames, figHandle)
-% implementation of the lovely https://upset.app/implementations/ in matlab 
+function bc_upSetPlot(setNames, Data, barColors)
+    % bc_upSetPlot creates an UpSet plot for visualizing set intersections
+    % and their sizes.
+    %
+    % Arguments:
+    %   setNames - Cell array of strings representing names of sets
+    %   Data - Binary matrix representing set memberships
+    %   barColors - Matrix of colors for bars
+    
+    % Define default colors if not provided
+    if nargin < 3 || isempty(barColors)
+        barColors = [66, 182, 195] ./ 255; % Default bar color
+    end
+    lineColor = [61, 58, 61] ./ 255; % Color for lines connecting sets
 
-% get sets 
+    % Generate all combinations of sets
+    pBool = abs(dec2bin((1:(2^size(Data, 2) - 1))') - 48);
 
-if param.computeDistanceMetrics && ~isnan(param.isoDmin)
-    setsw = {find(qMetric.nPeaks > param.maxNPeaks), find(qMetric.nTroughs > param.maxNTroughs), ...
-        find(qMetric.waveformBaselineFlatness >= param.maxWvBaselineFraction), ...
-        find(qMetric.spatialDecaySlope <= param.minSpatialDecaySlope), ...
-        find(qMetric.waveformDuration_peakTrough < param.minWvDuration | qMetric.waveformDuration_peakTrough > param.maxWvDuration), ...
-        find(qMetric.isSomatic == 0), find(qMetric.RPV_tauR_estimate > param.maxRPVviolations), ...
-        find(qMetric.percentageSpikesMissing_gaussian > param.maxPercSpikesMissing), ...
-        find(qMetric.nSpikes <= param.minNumSpikes), find(qMetric.rawAmplitude <= param.minAmplitude), ...
-        find(qMetric.signalToNoiseRatio <= param.minSignalToNoiseRatio), find(qMetric.maxDriftEstimate <= param.maxDrift),...
-        find(qMetric.isoD <= param.isoDmin), find(qMetric.Lratio >= param.lratioMax)};
-else
-    setsw = {find(qMetric.nPeaks > param.maxNPeaks), find(qMetric.nTroughs > param.maxNTroughs), ...
-        find(qMetric.waveformBaselineFlatness >= param.maxWvBaselineFraction), ...
-        find(qMetric.spatialDecaySlope <= param.minSpatialDecaySlope), ...
-        find(qMetric.waveformDuration_peakTrough < param.minWvDuration | qMetric.waveformDuration_peakTrough > param.maxWvDuration), ...
-        find(qMetric.isSomatic == 0), find(qMetric.RPV_tauR_estimate> param.maxRPVviolations), ...
-        find(qMetric.percentageSpikesMissing_gaussian > param.maxPercSpikesMissing), ...
-        find(qMetric.nSpikes <= param.minNumSpikes), find(qMetric.rawAmplitude <= param.minAmplitude),...
-        find(qMetric.signalToNoiseRatio <= param.minSignalToNoiseRatio), find(qMetric.maxDriftEstimate <= param.maxDrift)};
+    % Find positions for the UpSet plot
+    [pPos, ~] = find(((pBool * (1 - Data')) | ((1 - pBool) * Data')) == 0);
+    sPPos = sort(pPos);
+    dPPos = find([diff(sPPos); 1]);
+    pType = sPPos(dPPos);
+    pCount = diff([0; dPPos]);
+    [pCount, pInd] = sort(pCount, 'descend');
+    pType = pType(pInd);
+    
+    % Calculate the sum and sort the set counts
+    sCount = sum(Data, 1);
+    [sCount, sInd] = sort(sCount, 'descend');
+    sType = 1:size(Data, 2);
+    sType = sType(sInd);
+
+    % Create figure and axes
+    fig = figure('Units', 'normalized', 'Position', [.3, .2, .5, .63], 'Color', [1, 1, 1]);
+    % Axes for intersection plot
+    axI = setupAxes(fig, [.33, .35, .655, .61], 'Intersection Size', []);
+    % Axes for set size plot
+    axS = setupAxes(fig, [.01, .08, .245, .26], 'Set Size', 1:size(Data, 2));
+    % Axes for lines
+    axL = setupAxes(fig, [.33, .08, .655, .26], '', []);
+    
+    % Plot intersection sizes
+    plotBarGraph(axI, pCount, barColors);
+    % Plot set sizes
+    plotBarGraph(axS, sCount, barColors, setNames, sInd);
+    % Draw lines connecting sets and intersections
+    drawLines(axL, pBool, pType, sType, lineColor);
+    
 end
 
-% get possible combinations 
-noiseCombinations = arrayfun(@(x) nchoosek(1:6,x), 1:6, 'UniformOutput', false);
-muaCombinations = arrayfun(@(x) nchoosek(7:size(setsw,2),x), 1:size(setsw,2)-7, 'UniformOutput', false);
+function ax = setupAxes(parent, position, label, yticks)
+    % Helper function to setup axes properties
+    ax = axes('Parent', parent, 'Position', position);
+    hold on;
+    set(ax, 'LineWidth', 1.2, 'Box', 'off', 'TickDir', 'out', ...
+        'FontName', 'Times New Roman', 'FontSize', 12, 'YColor', 'none', 'YTick', yticks);
+    ax.XLabel.String = label;
+    ax.XLabel.FontSize = 16;
+end
 
-% get counts for each combination
+function plotBarGraph(ax, data, colors, setNames, indices)
+    % Helper function to plot bar graphs
+    barHdl = bar(ax, data, 'BarWidth', .6, 'FaceColor', 'flat');
+    barHdl.EdgeColor = 'none';
+    barHdl.BaseLine.Color = 'none';
+    for i = 1:length(data)
+        barHdl.CData(i, :) = colors(mod(i, size(colors, 1)) + 1, :);
+        if nargin > 3
+            % Add set names as text boxes
+            text(ax, data(i), i, setNames{indices(i)}, ...
+                'HorizontalAlignment', 'center', 'VerticalAlignment', 'bottom', ...
+                'FontName', 'Times New Roman', 'FontSize', 12);
+        end
+    end
+end
 
+function drawLines(ax, pBool, pType, sType, color)
+    % Helper function to draw lines between bars and sets
+    [tX, tY] = meshgrid(1:length(pType), 1:size(pBool, 2));
+    for i = 1:length(pType)
+        tY = find(pBool(pType(i), :));
+        oY = arrayfun(@(y) find(sType == y), tY);
+        tX = i .* ones(size(tY));
+        plot(ax, tX(:), oY(:), '-o', 'Color', color, ...
+            'MarkerEdgeColor', 'none', 'MarkerFaceColor', color, ...
+            'MarkerSize', 10, 'LineWidth', 2);
+    end
+end
