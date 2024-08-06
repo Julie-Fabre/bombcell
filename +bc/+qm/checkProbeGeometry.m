5dc47afdf0db1d2214d7c22562aaa34b26f8c9b9
Julie-Fabre <julie.m.fabre@gmail.com>
2024-08-06 16:33:21 +0100

    only compute spatial decay if probe geometry allows

--- +bc/+qm/checkProbeGeometry.m
+++ +bc/+qm/checkProbeGeometry.m
@@ -2,7 +2,7 @@ function computeSpatialDecay = checkProbeGeometry(channelPositions)
 % check if there are enough close-by channels to compute spatial decay
 % (i.e. is it a high density probe or not) 
 
-if min(diff(channelPositions(:, 2))) < 30
+if min(diff(unique(channelPositions(:, 2)))) < 30
     computeSpatialDecay = 1;
 else
     computeSpatialDecay = 0;
