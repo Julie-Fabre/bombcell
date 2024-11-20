f8ecb67927039abce9ea67feb44f316c7f0e99a8
Julie-Fabre <julie.m.fabre@gmail.com>
2024-11-20 16:13:40 +0000

    correct gain for channels - spikeGLX

--- +bc/+load/readSpikeGLXMetaFile.m
+++ +bc/+load/readSpikeGLXMetaFile.m
@@ -13,98 +13,30 @@ function [scalingFactor, channelMapImro, probeType] = readSpikeGLXMetaFile(metaF
 % microvolts
 %
 
-% read meta file
-filetext = fileread(metaFile);
+meta = bc.dependencies.SGLX_readMeta.ReadMeta(metaFile);
 
-% try and get probe type from meta file (imDatPrb_type or imProbeOpt fields)
-expr_scaling = 'imDatPrb_type=*';
-[~, startIndex] = regexp(filetext, expr_scaling);
-if isempty(startIndex) % try second option: there are two different saving conventions
-    expr_scaling = 'imProbeOpt=*';
-    [~, startIndex] = regexp(filetext, expr_scaling);
-end
-if isempty(startIndex) % if still no probe type information is found, use the param value
-    if strcmp(probeType, 'NaN')
-        error(['no probe type found in spikeGLX meta file and no param.probeType specified. ', ...
-            'Edit the param.probeType value in bc_qualityParamValues.'])
-    end
-else
-    endIndex = find(filetext(startIndex:end) == newline, 1, 'first') + startIndex - 2;
-    if isempty(endIndex)
-        endIndex = length(filetext);
-    end
-    probeType = filetext(startIndex+1:endIndex-1);
-end
-
-% get channel map information
-expr_chanMap = 'imRoFile=';
-[~, startIndexChanMap] = regexp(filetext, expr_chanMap);
-expr_afterChanMap = 'imSampRate';
-[~, endIndexChanMap] = regexp(filetext, expr_afterChanMap);
-if isempty(startIndexChanMap) % new convention in new spike glx argh
-    expr_chanMap = 'imroFile=';
-    [~, startIndexChanMap] = regexp(filetext, expr_chanMap);
-    expr_afterChanMap = 'nDataDirs';
-    [~, endIndexChanMap] = regexp(filetext, expr_afterChanMap);
-end
-
-channelMapImro = filetext(startIndexChanMap+1:endIndexChanMap-2-length(expr_afterChanMap));
+% channelMapImro 
+channelMapImro = meta.imRoFile;
 if isempty(channelMapImro) % default was used
     if strcmp(probeType, '0')
         channelMapImro = 'NPtype21_bank0_ref0';
     end
 end
 
-% get bits_encoding
-expr_imMax = 'imMaxInt=';
-[~, startIndeximMax] = regexp(filetext, expr_imMax);
-if isempty(startIndeximMax) % new convention in new spike glx argh
-    if ismember(probeType, {'1', '3', '0', '1020', '1030', '1100', '1120', '1121', '1122', '1123', '1200', '1300', '1110'}) %NP1, NP1-like
-        bits_encoding = 2^10; % 10-bit analog to digital
-    elseif ismember(probeType, {'21', '2003', '2004', '24', '2013', '2014', '2020'}) % NP2, NP2-like
-        bits_encoding = 2^14; % 14-bit analog to digital
-    else
-        error('unrecognized probe type. Check the imDatPrb_type value in your meta file and create a github issue / email us to add support for this probe type')
-    end
-else
-    endIndex = find(filetext(startIndeximMax:end) == newline, 1, 'first') + startIndeximMax - 2;
-    if isempty(endIndex)
-        endIndex = length(filetext);
-    end
-    bits_encoding = str2num(filetext(startIndeximMax+1:endIndex-1));
-end
+% probeType 
+probeType = meta.imDatPrb_type;
 
-% get
-expr_vMax = 'imAiRangeMax =';
-[~, startIndexvMax] = regexp(filetext, expr_vMax);
-if isempty(startIndexvMax) % new convention in new spike glx argh
-    if ismember(probeType, {'1', '3', '0', '1020', '1030', '1100', '1120', '1121', '1122', '1123', '1200', '1300', '1110'}) %NP1, NP2-like
-        Vrange = 1.2e6; % from -0.6 to 0.6 V = 1.2 V = 1.2 e6 microvolts
-    elseif ismember(probeType, {'21', '2003', '2004', '24', '2013', '2014', '2020'}) % NP2, NP2-like
-        Vrange = 1e6; % from -0.5 to 0.5 V = 1 V = 1 e6 microvolts
-    else
-        error('unrecognized probe type. Check the imDatPrb_type value in your meta file and create a github issue / email us to add support for this probe type')
-    end
-else
-    endIndex = find(filetext(startIndexvMax:end) == newline, 1, 'first') + startIndexvMax - 2;
-    if isempty(endIndex)
-        endIndex = length(filetext);
-    end
-    Vrange = 2 * str2num(filetext(startIndexvMax+1:endIndex-1)) * 1e6;
-end
+%% scaling factor 
+% gain 
+gain_allChannels = bc.dependencies.SGLX_readMeta.ChanGainsIM(meta);
+allChannels_index = bc.dependencies.SGLX_readMeta.OriginalChans(meta);
+thisChannel = find(allChannels_index == peakChan);
+thisGain = gain_allChannels(thisChannel);
 
-% gain - QQ read out from table, modify. this could be wrong in some cases.
-%
-if ismember(probeType, {'1', '3', '0', '1020', '1030', '1100', '1120', '1121', '1122', '1123', '1200', '1300', '1110'}) %NP1, NP2-like
-    gain = 500; % 10-bit analog to digital
-elseif ismember(probeType, {'21', '2003', '2004', '24'}) % NP2, NP2-like
-    gain = 80;
-elseif ismember(probeType, {'2013', '2014', '2020'}) % NP2, NP2-like
-    gain = 100; % 14-bit analog to digital
-else
-    error('unrecognized probe type. Check the imDatPrb_type value in your meta file and create a github issue / email us to add support for this probe type')
-end
+% Vrange / bits_encoding 
+fI2V = bc.dependencies.SGLX_readMeta.Int2Volts(meta);
 
 % calculate scaling factor
-scalingFactor = Vrange / bits_encoding / gain;
+scalingFactor = fI2V / thisGain;
+
 end
