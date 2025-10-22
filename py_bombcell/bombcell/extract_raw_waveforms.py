ca77861523ba616dd263bf2080cdb5717dfa0b27
Nikhil Chandra <nikhil@plexon.com>
2025-10-22 13:29:00 -0500

    fix: added try/except block for param['joblib_backend_preference'] in extract_raw_waveforms.py to prevent breaks for users who maintain their own copy of default_parameters.py

--- py_bombcell/bombcell/extract_raw_waveforms.py
+++ py_bombcell/bombcell/extract_raw_waveforms.py
@@ -524,7 +524,11 @@ def extract_raw_waveforms(
                 all_spikes_idxs[i, : len(clus_spike_times[i])] = clus_spike_times[i]
                 all_spikes_idxs[i, len(clus_spike_times[i]) :] = np.nan
 
-        prefer = param["joblib_backend_preference"]
+        try:
+            prefer = param["joblib_backend_preference"]
+        except KeyError:
+            param["joblib_backend_preference"] = "processes"
+            prefer = param["joblib_backend_preference"]
         all_waveforms = Parallel(n_jobs=-1, verbose=10, mmap_mode="r", max_nbytes=None, prefer=prefer)(
             delayed(process_a_unit)(
                 raw_data,
